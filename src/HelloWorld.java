c52013844b25f12c0aa089cc34fceb78a241f900
KevinJ6219 <kevinj66@nycstudents.net>
2022-09-28 08:29:13 -0400

    Added a print message

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class HelloWorld {
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is your name: ");
+        String name = input.nextLine();
+        System.out.println("Hello " + name );
+        System.out.println("THis is your first program o Intellij!");
+
+    }
 }
